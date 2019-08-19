23af3ece74c4f069ea0be2787d9ca8441aa0ecd5
dexlesshermit <666dexlesshermit67@gmail.com>
2019-08-19 14:45:17 +0300

    Added some features

--- s3_inspector.py
+++ s3_inspector.py
@@ -2,48 +2,101 @@
 @Author Aviad Shimoni
 @Date   18/08/2019
 '''
-
+import argparse
 import requests
 import boto3
-import botocore
+import datetime
 from botocore import endpoint
 from botocore.exceptions import ClientError
+import humanfriendly
+
+STRING = 'a'
 
 
-# import boto.s3.connection
+class Inspector:
 
-def inspector_curl(url='http://127.0.0.1:8001', timeout=5):
-    try:
-        _ = requests.get(url, timeout=timeout)
-        print("CURL IS UP!")
-        return True
-    except requests.ConnectionError:
-        print("s3 service is not reachable.")
-        return False
+    def __init__(self):
+        # creates all needed arguments for the program to run
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-e', '--endpoint-url', help="endpoint url for s3 object storage", required=True)
+        parser.add_argument('-a', '--access-key', help='access key for s3 object storage', required=True)
+        parser.add_argument('-s', '--secret-key', help='secret key for s3 object storage', required=True)
+
+        # parsing all arguments
+        args = parser.parse_args()
+
+        # building instance vars
+        self.endpoint_url = args.endpoint_url
+        self.access_key = args.access_key
+        self.secret_key = args.secret_key
+        self.bucket_name = 'inspector'
+        self.object_size = '1MB'
+        self.object_name = 'inspector_test_object'
+        self.s3 = boto3.client('s3', endpoint_url=self.endpoint_url, aws_access_key_id=self.access_key,
+                               aws_secret_access_key=self.secret_key)
+
+    def time_operation(self, method, name, bin_data):
+        if method == 'GET':
+            start = datetime.datetime.now()
+            self.get_object(name)
+            end = datetime.datetime.now()
+            diff = (end - start).total_seconds() * 1000
+
+        elif method == 'PUT':
+            start = datetime.datetime.now()
+            self.put_object(name, bin_data)
+            end = datetime.datetime.now()
+            diff = (end - start).total_seconds() * 1000
+
+        return diff
+
+    def inspector_curl(self):
+        try:
+            _ = requests.get(self.endpoint_url, timeout=5)
+            print("S3 IS UP!")
+            return True
+        except requests.ConnectionError:
+            print("S3 service is not reachable.")
+            return False
+
+    def put_object(self, name, bin_data):
+        self.s3.put_object(Key=object_name, Bucket=self.bucket_name, Body=bin_data)
+
+    def create_bin_data(self):
+        return humanfriendly.parse_size(self.object_size) * STRING
+
+    def get_object(self, name):
+        response = self.s3.get_object(Bucket=self.bucket_name, Key=object_name)
+        response['Body'].read()
 
 
 if __name__ == '__main__':
-    access_key = 'RK224EIAHPE2BH7ZYIQW'  # Add your S3 Access Key here
-    secret_key = 'QoHW1yt4OpvMjpnOuLse7cN3jKTgqz34cLMnzIsN'  # Add your S3 Secret Key here
-    bucket_name = 'inspector'
-    service_state = inspector_curl()
-    s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key,
-                      endpoint_url='http://127.0.0.1:8001')
-    print(s3)
-    s3.create_bucket(Bucket='inspector_bucket')
-    'Bucket {} created!'.format("inspector")
 
-    # declare some variables
-    key = 'home/user/Desktop/inspector_check_upload'
-    outputName = 'inspector_uploaded.txt'
+    # Creates an instance
+    inspector = Inspector()
+
+    # Creates a bucket for our script, if bucket's already exists, prints a message.
+    try:
+        inspector.s3.create_bucket(Bucket='inspector_bucket')
+        'Bucket {} created!'.format("inspector")
+    except ClientError:
+        print("Bucket already exist, No need to create.")
+
+    # Creates binary data
+    data = Inspector.create_bin_data()
+    # sets the object's name
+    object_name = 'inspector_test_object'
 
     # test upload
-    s3.upload_file('/home/user/Desktop/inspector_check_upload', "inspector", 'inspector_uploaded.txt')
+    inspector.put_object(object_name, bin_data=data)
+
     # test download
-    try:
-        s3.Bucket('inspector').download_file(key, outputName)
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print("The object does not exist.")
-        else:
-            raise
+    inspector.get_object(object_name, bin_data=data)
+
+    # True of False, checks curl
+    curl_check = inspector.inspector_curl()
+
+    # checks for get and put latency
+    get_latency = inspector.time_operation('GET', object_name, "")
+    put_latency = inspector.time_operation(('PUT', object_name, ""))
+
