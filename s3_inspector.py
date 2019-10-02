ebba1cdaad61d37d9e5c6bd1066530f52cb47d7f
aviadshimoni <shimoniaviad@gmail.com>
2019-10-02 17:34:18 +0300

    added cachet api ops

--- s3_inspector.py
+++ s3_inspector.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 '''
 @Author Aviad Shimoni
 @Date   18/08/2019
@@ -9,6 +11,10 @@ import datetime
 from botocore import endpoint
 from botocore.exceptions import ClientError
 import humanfriendly
+import cachetclient
+from cachetclient.v1 import enums
+from cachetclient.v1 import components
+
 
 STRING = 'a'
 
@@ -21,15 +27,17 @@ class Inspector:
         parser.add_argument('-e', '--endpoint-url', help="endpoint url for s3 object storage", required=True)
         parser.add_argument('-a', '--access-key', help='access key for s3 object storage', required=True)
         parser.add_argument('-s', '--secret-key', help='secret key for s3 object storage', required=True)
-       
+        parser.add_argument('-c', '--cachet-token', help='secret key for cachet client status page', required=True)
         # parsing all arguments
         args = parser.parse_args()
 
         # building instance vars
-        self.statuspageurl = 
+        self.statuspage = 'http://status.test/api/v1'
+
         self.endpoint_url = args.endpoint_url
         self.access_key = args.access_key
         self.secret_key = args.secret_key
+        self.cachet_token = args.cachet_token
         self.bucket_name = 'inspector_bucket'
         self.object_size = '1MB'
         self.object_name = 'inspector_test_object'
@@ -55,9 +63,10 @@ class Inspector:
     def inspector_curl(self):
         try:
             _ = requests.get(self.endpoint_url, timeout=5)
-            print("S3 Service is UP")
-            
+            return True
         except requests.ConnectionError:
+            print "Connection Error"
+            return False
             
 
     # Puts object on the s3
@@ -72,14 +81,30 @@ class Inspector:
     # Creates data from the memory for the Ceph object
     def create_bin_data(self):
         return humanfriendly.parse_size(self.object_size) * STRING
+    
+    def cachet_connect():
+        client = cachetclient.Client(
+            endpoint=self.statuspage
+            api_token=self.cachet_token
+            return client
+        )
 
-    #def notifytostatuspage(self, requestcheck, g_latency, p_latency, p_object, g_object):
+    def notify_cachet_curl(Client):
+        Client.ComponentManager.update(component_id=1, status=4)
 
+    def notify_cachet_get_performance(Client):
+        Client.ComponentManager.update(component_id=2, status=4)
+
+    def notify_cachet_get_performance(Client):
+        Client.ComponentManager.update(component_id=3, status=4)
+    
 
 if __name__ == '__main__':
 
     # Creates an instance
     inspector = Inspector()
+
+    cachet_connection = cachet_connect()
 
     # Creates a bucket for our script, if bucket's already exists, prints a message.
     try:
@@ -102,10 +127,21 @@ if __name__ == '__main__':
     # True of False, checks curl
     curl_check = inspector.inspector_curl()
 
+    if !curl_check:
+        notify_cachet_curl(cachet_connection)
+
     # checks for get and put latency
     get_latency = inspector.time_operation('GET', object_name, "")
     put_latency = inspector.time_operation('PUT', object_name, "")
 
-    print(get_latency)
-    print(put_latency)
     # compares between the latency and the threshold
+    if ( get_latency >= 2 ):
+        notify_notify_cachet_get_performance(cachet_connection)
+    else:
+        #make it green
+        #sending to status page function
+    
+    if ( put_latency >= 2 ):
+        notify_notify_cachet_put_performance(cachet_connection)
+   
+    
