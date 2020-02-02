7afe66a20250c53dc56c9208afa73750d94a680b
Aviad Shimoni <shimoniaviad@gmail.com>
2020-02-02 14:03:17 +0200

    added config file

---
+++ inspector_build/config.py
@@ -0,0 +1,8 @@
+# Config file which copied inside the container, used by the inspector script for vars configuration
+###Written by Aviad Shimoni - NTMM37###
+apitoken = "" #apitoken of cachet, get it by click on the profile picture in the Cachet UI.
+access = "" #access key used for s3
+secret = "" #secret key used for s3
+endpoint_url = "" #endpoint to a ceph cluster
+redis_host = "" #infranode
+cachet_host = "" #infranode
